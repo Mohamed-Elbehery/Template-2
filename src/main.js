69655bb780703c44ec9a9ac0f997acde2201453f
El-Behery <dev.elbehery@gmail.com>
2023-02-13 02:54:31 +0200

    finished

--- src/main.js
+++ src/main.js
@@ -26,13 +26,15 @@ const applyingAnimations = () => {
   if(window.pageYOffset >= landingSection.offsetTop) {
     landingSection.style.opacity = 1;
     landingSection.style.transform = 'translateY(0)';
-    goUp.style.display = 'none';
+    goUp.style.pointerEvents = 'none';
+    goUp.style.opacity = '0';
   }
   
   if(window.pageYOffset >= intro.offsetTop - 150) {
     intro.style.opacity = 1;
     intro.style.transform = 'translateY(0)';
-    goUp.style.display = 'block';
+    goUp.style.pointerEvents = 'visible';
+    goUp.style.opacity = '1';
   }
 
   if(window.pageYOffset >= ourWork.offsetTop - 200) {
@@ -51,26 +53,13 @@ const applyingAnimations = () => {
   }
 }
 
-window.onload = () => {
-  applyingAnimations();
-}
-
-
-window.addEventListener('scroll', () => {
-  applyingAnimations();
-})
-
-goUp.addEventListener('click', () => {
-  window.scroll(0, 0);
-});
-
 //! Dark and Light Mode
 const lightMode = document.querySelector('.light-mode');
 const darkMode = document.querySelector('.dark-mode');
 const primaryBtn = document.querySelectorAll('.btn-primary');
 const landingTitle = document.querySelector('.landing-content h2');
 
-// applying the dark mode
+// Applying the Dark Mode
 const darkModeChanges = () => {
   localStorage.theme = 'dark';
   document.body.style.backgroundColor = '#333';
@@ -97,6 +86,7 @@ const darkModeChanges = () => {
   })
 };
 
+// Applying the Light Mode
 const lightModeChanges = () => {
   localStorage.theme = 'light';
   document.body.style.backgroundColor = 'transparent';
@@ -121,7 +111,7 @@ const lightModeChanges = () => {
   header.children[0].children[1].children[0].addEventListener('mouseleave', () => {
     header.children[0].children[1].children[0].style.color = 'rgb(87, 114, 236)';
   })
-}
+};
 
 if(localStorage.theme) { // If there is a theme in local storage
   if(localStorage.theme === 'dark') { // Dark Mode
@@ -129,9 +119,21 @@ if(localStorage.theme) { // If there is a theme in local storage
   } else { // Light Mode
     lightModeChanges();
   }
-} else { // If there is no theme in the local storage
-  localStorage.setItem('theme', 'light');
-}
+} else // If there is no theme in the local storage
+  { localStorage.setItem('theme', 'light'); }
 
+//! Event Listeners and Events
 darkMode.addEventListener('click', darkModeChanges);
 lightMode.addEventListener('click', lightModeChanges);
+
+window.onload = () => {
+  applyingAnimations();
+};
+
+window.addEventListener('scroll', () => {
+  applyingAnimations();
+});
+
+goUp.addEventListener('click', () => {
+  window.scroll(0, 0);
+});
